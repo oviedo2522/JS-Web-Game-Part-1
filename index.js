4e48aaa7d7543e56339856ca0f9aa2e00ca9cc43
oviedo2522 <oviedo2522@gmail.com>
2024-03-16 09:24:37 -0700

    New Submission

--- index.js
+++ index.js
@@ -1,51 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
-
-function tile(url, left, bottom, width, height){
-    for(let h = 0; h < height; h++){
-        for(let w = 0; w < width; w++){
-            newImage(url, left + w*100, bottom+h*100)
-        }
-    }
+function newImage (url, left, bottom) {
+    let newAsset = document.createElement('img')
+    newAsset.src = url
+    newAsset.style.position = 'fixed'
+    newAsset.style.left = left + 'px'
+    newAsset.style.bottom = bottom + 'px'
+    document.body.append(newAsset)  
 }
 
-function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
-    return object
-}
+newImage("assets/green-character.gif",200, 300)
+newImage("assets/tree.png",350, 100)
+newImage("assets/crate.png",150, 200)
+newImage("assets/well.png", 500, 425)
 
-function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
+function clickImage (url, left, bottom){
+    let deletImage = document.createElement ("img")
+    deletImage.src = url 
+    deletImage.style.position = "fixed"
+    deletImage.style.left = left + "px"
+    deletImage.style.bottom = bottom + "px"
+    document.body.append(deletImage)
 
-    object.addEventListener('dblclick', () => {
-        object.remove()
+    deletImage.addEventListener("dblclick", function(){
+        deletImage.remove() 
     })
 }
 
-let horizon = window.innerHeight / 1.75
-let heightOfSky = window.innerHeight-horizon
-let heightOfGrass = horizon
-
-tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
-tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+clickImage ("assets/sword.png", 500, 405)
+clickImage ("assets/shield.png", 165, 185)
+clickImage ("assets/staff.png", 600, 100)
 
 
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
-
-newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
